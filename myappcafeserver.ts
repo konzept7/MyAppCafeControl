4ebd2a856696f989bcddfebe237e019efeabe4e3
Soeren Gruessinger <sgruessinger@ibs-ka.de>
2021-11-10 13:05:53 +0100

    debugging trashmove

--- myappcafeserver.ts
+++ myappcafeserver.ts
@@ -588,7 +588,8 @@ class Myappcafeserver extends EventEmitter implements ControllableProgram {
       if (!job.jobDocument.parameters || !("device" in job.jobDocument.parameters)) {
         throw new Error('no device defined')
       }
-      const response = await axios.post(this._url + 'robot/trash/' + job.jobDocument.parameters["device"], null, { timeout: 30 * 1000 });
+      //      const response = await axios.post(this._url + 'robot/trash/' + job.jobDocument.parameters["device"], null, { timeout: 30 * 1000 });
+      const response = await axios.post('http://192.168.155.17:5002/api/robot/trash/' + job.jobDocument.parameters["device"], null, { timeout: 30 * 1000 });
       if (response.status === 200) {
         jobUpdate(job.jobId, job.Succeed('trashmove for device successful'), this._thingName, this._connection)
         return
