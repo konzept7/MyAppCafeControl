6ecf225f5c64bccc2d7f148f9c5878f1b0d6b950
Frank Bielecke <frank.bielecke@gmail.com>
2023-08-04 13:05:26 +0200

    added echo for update script for better cron logs

--- scripts/update_myappcafecontrol.sh
+++ scripts/update_myappcafecontrol.sh
@@ -12,6 +12,7 @@ SCRIPTFILE=/etc/systemd/system/myappcafecontrol.service
 
 # check if service script exists, if not create it
 if [[ ! -f "$SCRIPTFILE" ]]; then
+    echo 'Creating service script...'
     # create script file
     cd /home/pi/
     sudo rm myappcafecontrol.service
@@ -27,7 +28,7 @@ if [[ ! -f "$SCRIPTFILE" ]]; then
     echo 'RestartSec=10' | sudo tee -a myappcafecontrol.service
     echo 'Restart=on-failure' | sudo tee -a myappcafecontrol.service
     echo 'StartLimitIntervalSec=60' | sudo tee -a myappcafecontrol.service
-    echo 'StartLimitBurst=10' | sudo tee -a myappcafecontrol.service
+    echo 'StartLimitBurst=100' | sudo tee -a myappcafecontrol.service
     echo 'User=pi' | sudo tee -a myappcafecontrol.service
     echo '' | sudo tee -a myappcafecontrol.service
     echo '[Install]' | sudo tee -a myappcafecontrol.service
@@ -43,24 +44,35 @@ fi
 
 # update service
 # first shutdown service
+echo 'Stopping service...'
 sudo systemctl stop myappcafecontrol.service
 # pull current version
 cd /home/pi/srv/MyAppCafeControl
+echo 'Pulling latest version...'
 git checkout .
 git pull origin master
+echo 'Installing dependencies...'
 npm install
 cd node_modules
 
 # download aws-crt if it does not exist
+echo 'Checking for aws-crt...'
 if [[ ! -d "/home/pi/dependencies/aws-crt/aws-crt" ]]; then
+    echo 'Downloading aws-crt...'
     mkdir -p /home/pi/dependencies/aws-crt
     cd /home/pi/dependencies/aws-crt
     wget https://s3.amazonaws.com/iot.myapp.cafe/public/aws-crt.zip
     unzip -o aws-crt.zip
 fi
 cd /home/pi/srv/MyAppCafeControl
+
+echo 'Copying aws-crt...'
 rm -rf node_modules/aws-crt
 cp -r /home/pi/dependencies/aws-crt/aws-crt node_modules/aws-crt
+
+echo 'Building...'
 npm run build
 # restart service after build
+
+echo 'Starting service...'
 sudo systemctl start myappcafecontrol.service
