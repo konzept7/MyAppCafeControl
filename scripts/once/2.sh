62221fe9525462513a1d8e9bf7ad2e2a20bf6b23
fbiel <frank.bielecke@googlemail.com>
2023-08-09 18:33:02 +0200

    added placeholder scripts

---
+++ scripts/once/2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+workdir=/home/pi/srv/MyAppCafeControl/scripts
+logfile=$workdir/once.log
+
+echo "$(date) MyAppCafé - Control - executing once script 2 - just a placeholder" >> $logfile
